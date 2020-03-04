ff96ee10e463c75946b8490091a954400b2968ff
ja-br <letstalk@jamesbrooks.tech>
2020-03-04 12:04:44 -0500

    Added clickCompanion and clickCompounder, reduce total clickcount when purchased

--- spec/js/ClickCounter.spec.js
+++ spec/js/ClickCounter.spec.js
@@ -1,9 +1,13 @@
 describe('Clicking Counter Calamity', () => {
     let underTest = new ClickCount();
+    beforeEach(() => {
+        underTest.clickCount = 0;
+        underTest.companionCount = 0;
+        underTest.compounderCount = 0;
+
+    })
     describe('countClick() records clicks and Clickcounter can give the clickCount', () => {
-        beforeEach(() => {
-            underTest.clickCount = 0;
-        })
+
         it('clickCount() 1 time should return 1', () => {
             underTest.click();
             expect(underTest.clickCount).toBe(1);
@@ -12,6 +16,37 @@ describe('Clicking Counter Calamity', () => {
             underTest.click();
             underTest.click();
             expect(underTest.clickCount).toBe(2);
+        });
+    });
+    describe('Clicking Companions', () => {
+
+        it('ClickCounter should begin with 0 companions', () => {
+            expect(underTest.getCompanionCount()).toBe(0);
+
+        });
+        it('purchaseCompanion should increase companioncount by 1', () => {
+            underTest.purchaseCompanion();
+            expect(underTest.getCompanionCount()).toBe(1);
+
+        });
+        it('purchaseCompanion reduces clickCount by 100', () => {
+            underTest.purchaseCompanion();
+            expect(underTest.clickCount).toBe(-100);
+        });
+
+    });
+    describe('Culmination Compounder', () => {
+
+        it('ClickCounter should begin with 0 compounders', () => {
+            expect(underTest.getCompounderCount()).toBe(0);
+        });
+        it('purchaseCompounder should increase compounderCount by 1', () => {
+            underTest.purchaseCompounder();
+            expect(underTest.getCompounderCount()).toBe(1);
+        });
+        it('purchaseCompanion reduces clickCount by 10', () => {
+            underTest.purchaseCompounder();
+            expect(underTest.clickCount).toBe(-10);
         });
     });
 
--- src/js/ClickCounter.js
+++ src/js/ClickCounter.js
@@ -1,8 +1,28 @@
 class ClickCount {
     constructor() {
         this.clickCount = 0;
+        this.companionCount = 0;
+        this.compounderCount = 0;
     }
     click() {
         this.clickCount++;
+    }
+
+    getCompanionCount() {
+        return this.companionCount;
+    }
+
+    purchaseCompanion() {
+        this.clickCount -= 100;
+        this.companionCount++;
+    }
+
+    getCompounderCount() {
+        return this.compounderCount;
+    }
+
+    purchaseCompounder() {
+        this.clickCount -= 10;
+        this.compounderCount++;
     }
 }
